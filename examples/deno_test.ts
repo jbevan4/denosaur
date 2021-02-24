0000fa3490de2fb9d4a005d8741d895f0bc417e0
Joshua-Luke Bevan <joshua-luke@hotmail.co.uk>
2021-02-24 22:50:12 +0000

    Added more deno testing

--- examples/deno_test.ts
+++ examples/deno_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "./deps.ts";
+import { assertArrayIncludes, assertEquals, delay } from "./deps.ts";
 
 // Simple name and function, compact form, but not configurable
 Deno.test("hello world #1", () => {
@@ -13,4 +13,21 @@ Deno.test({
     const x = 1 + 2;
     assertEquals(x, 3);
   },
+});
+
+Deno.test("hello world", () => {
+  const x = 1 + 2;
+  assertEquals(x, 3);
+  assertArrayIncludes([1, 2, 3, 4, 5, 6], [3], "Expected 3 to be in the array");
+});
+
+Deno.test("async hello world", async () => {
+  const x = 1 + 2;
+
+  // await some async task
+  await delay(600);
+
+  if (x !== 3) {
+    throw Error("x should be equal to 3");
+  }
 });
--- examples/deps.ts
+++ examples/deps.ts
@@ -1,4 +1,7 @@
 export {
   assert,
+  assertArrayIncludes,
   assertEquals,
 } from "https://deno.land/std@0.88.0/testing/asserts.ts";
+
+export { delay } from "https://deno.land/std@0.88.0/async/delay.ts";
